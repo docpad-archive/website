d5e0ce4e9c2292de2b3dd57a6564e7a5966b62e9
SteveMcArthur <contact@stevemcarthur.co.uk>
2016-03-08 08:57:07 +0000

    call to check url instead of request.get

--- tests/pages-exist.js
+++ tests/pages-exist.js
@@ -59,7 +59,7 @@ joe.suite("Home Page Tests", function (suite, test) {
     //the wrong page
     test("Home page Title is: 'DocPad", function (done) {
         var url = siteURL;
-        request.get(url).end(function (error, res) {
+        checkURL(url,function(error,statusCode,res){
             var $ = cheerio.load(res.text);
             var title = $('#brand h1').text().trim();
             isEqual(title, 'DocPad', 'Title is DocPad');
@@ -69,7 +69,7 @@ joe.suite("Home Page Tests", function (suite, test) {
 
     test("Home page has navigation menu", function (done) {
         var url = siteURL;
-        request.get(url).end(function (error, res) {
+        checkURL(url,function(error,statusCode,res){
             var $ = cheerio.load(res.text);
             var menu = $('#nav-menu');
             assert.equal(1, menu.length);
@@ -85,7 +85,7 @@ joe.suite("Menu Links Work", function (suite, test) {
 
     var linkList = [];
     test("Menu links exists", function (done) {
-        request.get(siteURL).end(function (error, res) {
+        checkURL(siteURL,function(error,statusCode,res){
             var $ = cheerio.load(res.text);
             var menu = $('#nav-menu');
             assert.equal(menu.length, 1, "Nav menu exists");
@@ -97,8 +97,8 @@ joe.suite("Menu Links Work", function (suite, test) {
     linkList.forEach(function (url) {
         var fullUrl = urlUtil.resolve(siteURL, url);
         test(fullUrl, function (done) {
-            request.get(fullUrl).end(function (error, res) {
-                assert.isEqual(res.statusCode, HTTP_OK, "status code");
+            checkURL(fullUrl,function(error,statusCode,res){
+                assert.isEqual(statusCode, HTTP_OK, "status code");
                 done();
             });
         });
@@ -112,7 +112,7 @@ joe.suite("Showcase Links Work", function (suite, test) {
     var showcaseUrl = urlUtil.resolve(siteURL, '/docs/showcase');
     var linkList = [];
     test("Showcase page exists", function (done) {
-        request.get(showcaseUrl).end(function (error, res) {
+        checkURL(showcaseUrl,function(error,statusCode,res){
             isEqual(res.statusCode, HTTP_OK, "status code");
             linkList = buildLinkList(res.text, 'article ul li a');
             done();
@@ -123,14 +123,8 @@ joe.suite("Showcase Links Work", function (suite, test) {
     suite("Test All Showcase Links", function (suite,test) {
             linkList.forEach(function (url) {
                 test(url, function (done) {
-                    request.get(url).end(function (error, res) {
- 
-                        if(res == null){
-                            isEqual(null, HTTP_OK, "status code");
-                        }
-                               
-       
-                        isEqual(res.statusCode, HTTP_OK, "status code");
+                    checkURL(url,function(error,statusCode,res){      
+                        isEqual(statusCode, HTTP_OK, "status code");
                         done();
                     });
                 });

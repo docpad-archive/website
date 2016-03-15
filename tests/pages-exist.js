3884479b0bed298b4235dcdb1d9ca1d9d18248dc
SteveMcArthur <contact@stevemcarthur.co.uk>
2016-03-15 13:32:35 +0000

    compact reporter now NPM package. assert-helpers source extracted so as to override logComparison. Chai replaced with assert-helpers.

--- tests/pages-exist.js
+++ tests/pages-exist.js
@@ -1,22 +1,20 @@
-/*global require, process*/
+/*global require*/
 "use strict"
 
 
 
 // Import
+var util = require("util");
 var joe = require("joe");
-//var assert = require("assert-helpers");
-var chai = require('chai');
-var assert = chai.assert;
+var assert = require("./assert-helpers");//needed to override the logComparison method
 var cheerio = require('cheerio');
 var urlUtil = require('url');
 var checkURL = require('./linkChecker').checkURL;
 
-var Reporter = require('./joe-reporter-compact');
+var Reporter = require('joe-reporter-compact');
 var compact = new Reporter();
 joe.setReporter(compact);
 
-assert.logComparison = function(){};
 
 // Prepare
 var HTTP_NOT_FOUND = 404;
@@ -42,31 +40,34 @@ function buildLinkList(html, selector) {
 joe.suite("Home Page Tests", function (suite, test) {
 
 
-    test("Home page exists", function () {
+    test("Home page exists", function (complete) {
         checkURL(siteURL, function (error, statusCode, res) {
             assert.equal(statusCode, HTTP_OK, "status code");
             buildLinkList(res.text, '#nav-menu li.menu-item a');
+            complete();
         });
     });
 
     //should really always check the content of the page
     //in someway as links and urls can sometimes lead to
     //the wrong page
-    test("Home page Title is: 'DocPad", function () {
+    test("Home page Title is: 'DocPad", function (complete) {
         var url = siteURL;
         checkURL(url, function (error, statusCode, res) {
             var $ = cheerio.load(res.text);
             var title = $('#brand h1').text().trim();
             assert.equal(title, 'DocPad', 'Title is DocPad');
+            complete();
         });
     });
 
-    test("Home page has navigation menu", function () {
+    test("Home page has navigation menu", function (complete) {
         var url = siteURL;
         checkURL(url, function (error, statusCode, res) {
             var $ = cheerio.load(res.text);
             var menu = $('#nav-menu');
             assert.equal(1, menu.length);
+            complete();
         });
     });
 
@@ -80,12 +81,13 @@ joe.suite("Menu Links Work", function (suite, test) {
     });
 
     var linkList = [];
-    test("Menu links exists", function () {
+    test("Menu links exists", function (complete) {
         checkURL(siteURL, function (error, statusCode, res) {
             var $ = cheerio.load(res.text);
             var menu = $('#nav-menu');
             assert.equal(menu.length, 1, "Nav menu exists");
             linkList = buildLinkList(res.text, '#nav-menu li.menu-item a');
+            complete();
         });
     });
 
@@ -95,10 +97,13 @@ joe.suite("Menu Links Work", function (suite, test) {
         });
         linkList.forEach(function (url) {
             var fullUrl = urlUtil.resolve(siteURL, url);
-            test(fullUrl, function () {
+            test(fullUrl, function (complete) {
                 checkURL(fullUrl, function (error, statusCode) {
-                    assert.isEqual(statusCode, HTTP_OK, "status code");
+                    
+                    assert.equal(statusCode, HTTP_OK, "status code");
+                    complete();
                 });
+                
             });
         });
 
@@ -120,6 +125,7 @@ joe.suite("Showcase Links Work", function (suite, test) {
             complete();
         });
     });
+    
 
     suite("Test All Showcase Links", function (suite, test) {
         this.setNestedConfig({

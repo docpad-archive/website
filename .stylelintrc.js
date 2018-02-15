3f1048fac73881be284b29b03b45912b8bf22a94
Benjamin Lupton <b@lupton.cc>
2018-02-15 17:42:48 +0800

    updated base files with boundation, updated deps

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,15 +2,17 @@
 // https://github.com/bevry/base
 // https://stylelint.io/user-guide/rules/
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
+	"extends": "stylelint-config-standard",
+	"rules": {
 		"indentation": "tab",
 		"rule-empty-line-before": null,
 		"custom-property-empty-line-before": null,
 		"at-rule-empty-line-before": null,
 		"declaration-empty-line-before": null,
 		"max-empty-lines": 2,
-		"selector-list-comma-newline-after": null
+		"selector-list-comma-newline-after": null,
+		"no-duplicate-selectors": null,
+		"no-descending-specificity": null
 	},
 	"ignoreFiles": ["**/vendor/*.css"]
 }
